1345fd46c298612b37114d19a579d9ba6ead553c
AlexSG1709 <programadoralex.github@gmail.com>
2026-06-09 07:30:11 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ public class Main {
     public static void main(String[] args) {
         Personagem heroi1 = new Personagem("Arthus", "Guerreiro", 1, 100, 10.5);
 
-       Personagem heroi2 = new Personagem("Arthemis", "Arqueira", 5, 80, 12.5);
+       Personagem heroi2 = new Personagem("Arthemis", "Mago", 5, 80, 12.5);
 
         //exibe o status de cada personagem
         heroi1.exibirStatus();
